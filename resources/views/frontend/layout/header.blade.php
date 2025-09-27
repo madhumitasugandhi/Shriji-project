667b2daf7f3ec7677681db3477bd247bbf2a79ca
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-09-27 14:07:13 +0530

    Add profile update forms for password and profile information, and create a welcome page layout

---
+++ resources/views/frontend/layout/header.blade.php
@@ -0,0 +1,46 @@
+<header id="header" class="header fixed-top">
+
+    <div class="topbar d-flex align-items-center dark-background">
+        <div class="container d-flex justify-content-center justify-content-md-between">
+            <div class="contact-info d-flex align-items-center">
+                <i class="bi bi-envelope d-flex align-items-center"><a
+                        href="mailto:contact@example.com">contact@example.com</a></i>
+                <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
+            </div>
+            <div class="social-links d-none d-md-flex align-items-center">
+                <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
+                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            </div>
+        </div>
+    </div><!-- End Top Bar -->
+
+    <div class="branding d-flex align-items-cente">
+
+        <div class="container position-relative d-flex align-items-center justify-content-between">
+            <a href="index.html" class="logo d-flex align-items-center">
+                <!-- Uncomment the line below if you also wish to use an image logo -->
+                <img src="assets/img/logo.jpg" alt="">
+                <h1 class="sitename">Shriji Brother's</h1>
+            </a>
+
+            <nav id="navmenu" class="navmenu">
+                <ul>
+                    <li><a href="/" class="active">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/products">Products</a></li>
+
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/login"><i class="bi bi-person text-md py-2 px-2 rounded-circle text-dark bg-white"></i></a></li>
+                    <li><a href="/register"><i class="bi bi-person-plus text-md py-2 px-2 rounded-circle text-dark bg-white"></i></a></li>
+                </ul>
+                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
+
+        </div>
+
+    </div>
+
+</header>
+
